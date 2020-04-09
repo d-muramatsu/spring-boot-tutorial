a54715028a47e67ef0c2d269b1ec7e4646c3a251
Daisuke Muramatsu <daisuke.muramatsu@gmail.com>
2020-04-09 18:47:33 +0900

    Google アカウントでログインする技術検証中

---
+++ src/main/java/com/example/springboottutorial/api/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.springboottutorial.api.controller;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+
+}
